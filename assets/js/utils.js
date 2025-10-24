cf133144ecc2944fb6b9c3d5ac12399afb9e185a
vvccnfrtms-pixel <vvccnfrtms@privaterelay.appleid.com>
2025-10-25 00:24:01 +0200

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,28 @@
+// ==============================
+// UTILS.JS
+// Shared helper functions
+// ==============================
 
+/**
+ * Create a DOM element with classes and optional innerHTML
+ * @param {string} tag - The HTML tag name
+ * @param {string[]} classes - Array of class names
+ * @param {string} innerHTML - Optional inner HTML
+ * @returns {HTMLElement}
+ */
+function createElement(tag, classes = [], innerHTML = '') {
+  const el = document.createElement(tag);
+  classes.forEach(cls => el.classList.add(cls));
+  if (innerHTML) el.innerHTML = innerHTML;
+  return el;
+}
+
+/**
+ * Check if an element exists before applying callback
+ * @param {string} selector - CSS selector
+ * @param {Function} callback - Function to run if element exists
+ */
+function ifExists(selector, callback) {
+  const el = document.querySelector(selector);
+  if (el) callback(el);
+}
